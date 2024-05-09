a5452656353eefe2c91b2c88e334e1cc63acdba8
BenBet21 <benoit.beti@gmail.com>
2024-05-09 16:11:24 +0200

    Write a program that prints the number of and the list of its arguments TASK 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
 
     if len(arguments) == 2:
         print("{} argument:".format(len(arguments) - 1))
-        for index, arg in enumerate(arguments[1:], start=1):        
+        for index, arg in enumerate(arguments[1:], start=1):
             print("{}: {}".format(index, arg))
 
-    if len(arguments) > 2:    
+    if len(arguments) > 2:
         print("{} arguments:".format(len(arguments) - 1))
-        for index, arg in enumerate(arguments[1:], start=1):        
+        for index, arg in enumerate(arguments[1:], start=1):
             print("{}: {}".format(index, arg))
