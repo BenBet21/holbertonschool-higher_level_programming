db9f3861b45f1b996ae9d4c7bbaca69570d717aa
BenBet21 <benoit.beti@gmail.com>
2024-05-15 14:49:07 +0200

    Write a function that prints x elements of a list. TASK 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed_elements = 0
+    for index, element in enumerate(my_list):
+        if printed_elements < x:
+            try:
+                print(element, end='')
+                printed_elements += 1
+            except:
+                pass
+    print()
+    return printed_elements
