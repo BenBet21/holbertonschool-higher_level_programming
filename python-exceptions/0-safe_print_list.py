d92d6a10093f098bea1b9d38fbaa582468225658
BenBet21 <benoit.beti@gmail.com>
2024-05-15 14:57:06 +0200

    Write a function that prints x elements of a list. TASK 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             try:
                 print(element, end='')
                 printed_elements += 1
-            except:
+            except TypeError:
                 pass
     print()
     return printed_elements
