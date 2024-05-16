ccd7d1026d02b4d6fcf388e28ae8153c262c7752
BenBet21 <benoit.beti@gmail.com>
2024-05-16 15:48:37 +0200

    Write a class Square that defines a square by: (based on 0-square.py) TASK 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''This is a Square Module'''
+
+
 class Square:
     '''this is a class square'''
-    pass
+    def __init__(self, size):
+        '''this is the cosntructeur'''
+        self.__size = size
