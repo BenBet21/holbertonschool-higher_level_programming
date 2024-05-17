6376d48b1fbb297dc952339733f677eff1dc1fcd
BenBet21 <benoit.beti@gmail.com>
2024-05-17 11:15:15 +0200

    Write a class Square that defines a square by TASK 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+'''This is a Square Module'''
+
+
+class Square:
+    '''this is a that defines a square'''
+    def __init__(self, size=0):
+        '''this is the constructeur'''
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''this a public methode to calculate the square area'''
+        return self.__size ** 2
