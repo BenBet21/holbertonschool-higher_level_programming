53ec5ce3d9c415be4516d010da62a51a2dffcf30
BenBet21 <benoit.beti@gmail.com>
2024-05-21 13:54:27 +0200

    Write a function that prints My name is <first name> <last name> TASK 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,30 +9,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     print("My name is {} {}".format(first_name, last_name))
-
-
-
-
-
-    """for row in matrix:
-        for nb in row:
-            if not isinstance(nb, (int, float)):
-                raise TypeError("matrix must be a matrix \
-(list of lists) of integers/floats")
-
-    row_length = None
-    for row in matrix:
-        if row_length is None:
-            row_length = len(row)
-        elif len(row) != row_length:
-            raise TypeError("Each row of the matrix \
-must have the same size")
-
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    return [[round(element / div, 2) for element in row] for row in matrix]"""
