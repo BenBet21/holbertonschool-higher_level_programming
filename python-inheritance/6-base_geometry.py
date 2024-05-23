8af325a9827e9f44f6b9bff3c8aefa2f81550b34
BenBet21 <benoit.beti@gmail.com>
2024-05-23 15:13:56 +0200

    Write a class BaseGeometry task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''This is Geometry Module'''
+
+
+class BaseGeometry:
+    '''this is a class BaseGeometry'''
+
+    def area(self):
+        raise Exception("area() is not implemented")
