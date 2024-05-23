01100cade77376ab1092e1466760fa616a19212b
BenBet21 <benoit.beti@gmail.com>
2024-05-23 11:46:50 +0200

    Write a class MyList that inherits from list: <TASK 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,5 +5,6 @@
 class MyList(list):
     """this class inherits from list"""
     def print_sorted(self):
-        print(sorted(self))
-
+        new_list = sorted(self)
+        print(new_list)
+        return new_list
