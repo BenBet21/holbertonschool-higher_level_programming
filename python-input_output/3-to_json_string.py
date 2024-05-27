dd1b6086c316dcf0eb4f3316e4c14f8ae964ba63
BenBet21 <benoit.beti@gmail.com>
2024-05-27 11:27:48 +0200

    3. To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This function returns the JSON representation \
+of an object (string):"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """3. To JSON string"""
+    return json.dumps(my_obj)
