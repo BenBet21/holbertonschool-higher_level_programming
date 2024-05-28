0b6b12481a2e468e8e46da346a8e9559229ebb78
BenBet21 <benoit.beti@gmail.com>
2024-05-28 09:30:22 +0200

    0. Basic Serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Python Module to demonstrate basic serialization and deserialization of data."""
-
+"""Python Module to demonstrate \
+basic serialization and deserialization of data."""
 import json
+
 
 def serialize_and_save_to_file(data, filename):
     """Serialize and save data to the specified file."""
 
     with open('filename', 'w', encoding="utf-8") as file:
         json.dump(data, file)
+
 
 def load_and_deserialize(filename):
     """Load and deserialize data from the specified file."""
