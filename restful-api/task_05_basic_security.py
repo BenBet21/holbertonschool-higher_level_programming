9047be37ae20b68bc338448e75b70c80c0c94877
BenBet21 <benoit.beti@gmail.com>
2024-05-31 14:42:58 +0200

    5. API Security and Authentication Techniques

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -12,22 +12,22 @@ from functools import wraps
 SECRET_KEY = "Last_Christmas"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config['JWT_SECRET_KEY'] = SECRET_KEY
+app.config['JWT_SECRET_KEY'] = SECRET_KEY  # Utilisé par Flask-JWT-Extended
 auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 
 users = {
-      "user1": {
-        "username": "Benoit",
-        "password": generate_password_hash("passworduser"),
+    "user1": {
+        "username": "user1",
+        "password": generate_password_hash("password"),
         "role": "user"
-        },
-      "admin1": {
-        "username": "Ben",
-        "password": generate_password_hash("passwordadmin"),
+    },
+    "admin1": {
+        "username": "admin1",
+        "password": generate_password_hash("password"),
         "role": "admin"
-        }
-        }
+    }
+}
 
 
 @auth.verify_password
@@ -53,34 +53,28 @@ def login():
     username = data['username']
     password = data['password']
     user = users.get(username)
-
+    
     if user and check_password_hash(user['password'], password):
-        token = create_access_token(identity={'username': username,
-                                              'role': user['role']})
+        token = create_access_token(identity={'username': username, 'role': user['role']})
         return jsonify({'access_token': token})
 
-    return jsonify({'message': 'Could not verify'}), 401
+    return jsonify({'message': 'Invalid username or password'}), 401
 
 
 @app.route('/jwt-protected', methods=['GET'])
 @jwt_required()
 def jwt_protected():
     current_user = get_jwt_identity()
-    return jsonify({'message': f'JWT Auth: Access Granted,
-                    {current_user["username"]}'})
+    return jsonify({'message': f'JWT Auth: Access Granted, {current_user["username"]}'})
 
 
 def role_required(required_role):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            """Decorator function that checks if the \
-current user has the required role"""
-
             current_user = get_jwt_identity()
             if current_user['role'] != required_role:
-                return jsonify({
-                    'message': 'Access denied:insufficient permissions'}), 403
+                return jsonify({'message': 'Access denied: insufficient permissions'}), 403
             return f(*args, **kwargs)
         return decorated_function
     return decorator
@@ -91,8 +85,7 @@ current user has the required role"""
 @role_required('admin')
 def admin_route():
     current_user = get_jwt_identity()
-    return jsonify({
-        'message': f'Admin Access: Granted, {current_user["username"]}'})
+    return jsonify({'message': f'Admin Access: Granted, {current_user["username"]}'})
 
 
 # Gestionnaires d'erreurs JWT
@@ -100,21 +93,17 @@ def admin_route():
 def handle_unauthorized_error():
     return jsonify({"error": "Missing or invalid token"}), 401
 
-
 @jwt.invalid_token_loader
 def handle_invalid_token_error():
     return jsonify({"error": "Invalid token"}), 401
-
 
 @jwt.expired_token_loader
 def handle_expired_token_error():
     return jsonify({"error": "Token has expired"}), 401
 
-
 @jwt.revoked_token_loader
 def handle_revoked_token_error():
     return jsonify({"error": "Token has been revoked"}), 401
-
 
 @jwt.needs_fresh_token_loader
 def handle_needs_fresh_token_error():
