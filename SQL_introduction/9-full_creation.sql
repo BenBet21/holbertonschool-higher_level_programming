59af602ff1515cafa5e635b7e99317678f96aa8d
BenBet21 <benoit.beti@gmail.com>
2024-06-17 20:43:37 +0200

    9. Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- ===========================================
+-- Write a script that creates a table second_table in the database 
+-- auteur : BenBet21 17/06/2024
+-- ============================================
+
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT PRIMARY KEY,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table(id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
