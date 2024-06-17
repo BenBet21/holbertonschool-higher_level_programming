d92d4b1735b519944fcc759335d52c67d4774f76
BenBet21 <benoit.beti@gmail.com>
2024-06-17 16:01:16 +0200

    6. List all in table

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- ===========================================
+-- Write a script that lists all rows of the table first_table
+-- auteur : BenBet21 17/06/2024
+-- ============================================
+
+SELECT * FROM first_table;
