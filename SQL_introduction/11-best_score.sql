1ba815bdc879d38f315de37beb47ab3fe9a23248
BenBet21 <benoit.beti@gmail.com>
2024-06-17 21:33:23 +0200

    11. Select the best

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- ===========================================
+-- Write a script that lists all rows of the table first_table
+-- auteur : BenBet21 17/06/2024
+-- ============================================
+
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
