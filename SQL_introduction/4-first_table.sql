b13319596013e370acf61b8c862ce1517ea4d21b
BenBet21 <benoit.beti@gmail.com>
2024-06-17 15:27:59 +0200

    4. First table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,12 +1,6 @@
-#!/bin/bash
 -- ===========================================
 -- Write a script that creates a table called first_table in the current database
 -- auteur : BenBet21 17/06/2024
 -- ============================================
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <database_name>"
-  exit 1
-fi
-DB_NAME="$1"
-mysql -u your_username -p -D "$DB_NAME" -e "CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));"
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
