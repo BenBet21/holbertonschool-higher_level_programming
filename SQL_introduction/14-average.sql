d38d9ea71dc73f4d1afd8fd7e277a64b7c729031
BenBet21 <benoit.beti@gmail.com>
2024-06-17 22:34:33 +0200

    14. Average

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- ===========================================
+-- Write a script that computes the score average of all records in the table second_table
+-- auteur : BenBet21 17/06/2024
+-- ============================================
+
+SELECT AVG(score) AS average_score FROM second_table;
