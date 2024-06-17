6b4d11e12d23b019ed9b97e2f77f98f8ea18771a
BenBet21 <benoit.beti@gmail.com>
2024-06-17 21:28:44 +0200

    10. List by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- ===========================================
+-- Write a script that lists all rows of the table first_table
+-- auteur : BenBet21 17/06/2024
+-- ============================================
+
+SELECT score, name FROM second_table ORDER BY score DESC;
