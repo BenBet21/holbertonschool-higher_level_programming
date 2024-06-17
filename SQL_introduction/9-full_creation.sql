057c0fa441d2c6650580a70eab23930228918af2
BenBet21 <benoit.beti@gmail.com>
2024-06-17 21:13:13 +0200

    9. Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- ===========================================
+ip addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'-- ===========================================
 -- Write a script that creates a table second_table in the database 
 -- auteur : BenBet21 17/06/2024
 -- ============================================
