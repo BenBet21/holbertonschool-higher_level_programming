37f0899efca7c11c31625dae28b6f03d938df556
BenBet21 <benoit.beti@gmail.com>
2024-06-17 16:15:09 +0200

    8. Count 89

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- ===========================================
+-- Write a script that displays the number of records
+-- auteur : BenBet21 17/06/2024
+-- ============================================
+
+SELECT COUNT (89) FROM first_table;
