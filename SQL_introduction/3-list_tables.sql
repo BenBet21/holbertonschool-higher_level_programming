2da469e43c52bba1321c837a55356dcff15e814e
BenBet21 <benoit.beti@gmail.com>
2024-06-17 15:20:57 +0200

    3. List tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,9 +2,4 @@
 -- Write a script that lists all the tables of a database
 -- auteur : BenBet21
 -- ============================================
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <mysql>"
-  exit 1
-fi
-DB_NAME="$1"
-mysql -u your_username -p -e "USE $DB_NAME; SHOW TABLES;"
+SHOW TABLES;
