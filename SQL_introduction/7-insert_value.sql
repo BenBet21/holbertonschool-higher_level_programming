1c107ec53cf4142105818ce50b3af5d869284c64
BenBet21 <benoit.beti@gmail.com>
2024-06-17 16:09:13 +0200

    7. First add

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- ===========================================
+-- Write a script that inserts a new row in the table first_table
+-- auteur : BenBet21 17/06/2024
+-- ============================================
+
+INSERT INTO first_table(id, name) VALUES(89, Best School);
