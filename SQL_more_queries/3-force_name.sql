e0f0e6809808b7bda460a7e3aacbbb812f191a96
BenBet21 <benoit.beti@gmail.com>
2024-06-18 14:06:40 +0200

    3. Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -2,7 +2,6 @@
 -- Write a script that creates the table force_name
 -- auteur : BenBet21
 -- ============================================
-USE hbtn_0d_2;
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
