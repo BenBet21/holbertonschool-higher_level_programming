8a66cab39c033ba067346d09c317125e827e2a9d
BenBet21 <benoit.beti@gmail.com>
2024-06-21 09:53:21 +0200

    6. First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Class State that inherits from Base"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
