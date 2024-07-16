b652c97eeb5c7b4c91f5da1eb3136f51a21bc576
BenBet21 <benoit.beti@gmail.com>
2024-07-16 11:42:52 +0200

    4. Extending Dynamic Data Display to Include SQLite in Flask

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -91,11 +91,12 @@ def create_database():
     cursor.execute('''
         INSERT OR IGNORE INTO Products (id, name, category, price)
         VALUES
-        (6, 'Laptop', 'Electronics', 799.99),
-        (7, 'Coffee Mug', 'Home Goods', 15.99)
+        (1, 'Laptop', 'Electronics', 799.99),
+        (2, 'Coffee Mug', 'Home Goods', 15.99)
     ''')
     conn.commit()
     conn.close()
 
 if __name__ == '__main__':
     create_database()
+    app.run(debug=True, port=5000)
